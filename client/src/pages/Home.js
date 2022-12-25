d7234b275635b3db53fad9eed67cc6a17d748f3d
Shubham Poriya <shubhamporiya@gmail.com>
2022-12-25 19:18:05 -0400

    frontend routes added

---
+++ client/src/pages/Home.js
@@ -0,0 +1,16 @@
+import CakeList from "../components/Cakes/CakeList";
+import NewCakeForm from "../components/Cakes/NewCakeForm";
+import Header from "../components/Layout/Header";
+import SideNav from "../components/Layout/SideNav";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <CakeList />
+      <NewCakeForm />
+    </div>
+  );
+};
+
+export default Home;
